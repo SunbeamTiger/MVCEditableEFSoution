dd696f8a0df3f2a851c3906a57bdf7c9b3c1d0ef
dan.hagen <dan.hagen@centralia.edu>
2024-03-07 08:34:43 -0800

    Updated to 8

--- MVCAppWithEF/ViewModels/EditCustomerViewModel.cs
+++ MVCAppWithEF/ViewModels/EditCustomerViewModel.cs
@@ -6,9 +6,11 @@ namespace MVCAppWithEF.ViewModels
     public class EditCustomerViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Last First is required")]
+        [StringLength(40)]
         public string FirstName { get; set; } = null!;
         [Required(ErrorMessage = "Last Name is required")]
-        [StringLength(160)]
+        [StringLength(40)]
         public string LastName { get; set; } = null!;
         public string? City { get; set; }
         public string? Country { get; set; }
